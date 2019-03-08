e5e0841aabea82c84ce8931285ed132e8137e0eb
Joshua Boele <joshuaboele@MacBook-Pro-van-Jules.local>
2019-03-08 16:40:18 +0100

    Legenda, random colors population

--- js/main.js
+++ js/main.js
@@ -9,13 +9,81 @@ const projection = d3
 
 const path = d3.geoPath().projection(projection);
 
-const colorScheme = d3.scaleSequential(d3.interpolatePuBuGn);
+// tooltip init
+
+tip = d3
+    .tip()
+    .attr('class', 'd3-tip')
+    .html(function(d) {
+        return d.properties.geounit;
+    });
+
+const colorScheme = d3.scaleSequential(d3.interpolateBlues);
 
 const svg = d3
     .select('body')
     .append('svg')
     .attr('width', width)
-    .attr('height', height);
+    .attr('height', height)
+    .call(tip);
+
+const color = d3
+    .scaleThreshold()
+    .domain(d3.range(2, 10))
+    .range(d3.schemeBlues[9]);
+
+var g = svg
+    .append('g')
+    .attr('class', 'key')
+    .attr('transform', 'translate(0,40)');
+
+var x = d3
+    .scaleLinear()
+    .domain([1, 10])
+    .rangeRound([600, 860]);
+
+g.selectAll('rect')
+    .data(
+        color.range().map(function(d) {
+            d = color.invertExtent(d);
+            if (d[0] == null) d[0] = x.domain()[0];
+            if (d[1] == null) d[1] = x.domain()[1];
+            return d;
+        })
+    )
+    .enter()
+    .append('rect')
+    .attr('height', 8)
+    .attr('x', function(d) {
+        return x(d[0]);
+    })
+    .attr('width', function(d) {
+        return x(d[1]) - x(d[0]);
+    })
+    .attr('fill', function(d) {
+        return color(d[0]);
+    });
+
+g.append('text')
+    .attr('class', 'caption')
+    .attr('x', x.range()[0])
+    .attr('y', -6)
+    .attr('fill', '#000')
+    .attr('text-anchor', 'start')
+    .attr('font-weight', 'bold')
+    .text('Unemployment rate');
+
+g.call(
+    d3
+        .axisBottom(x)
+        .tickSize(13)
+        .tickFormat(function(x, i) {
+            return i ? x : x + '%';
+        })
+        .tickValues(color.domain())
+)
+    .select('.domain')
+    .remove();
 
 d3.json('source/continent_Africa_subunits.json').then(function(africa) {
     console.log(africa);
@@ -28,11 +96,6 @@ d3.json('source/continent_Africa_subunits.json').then(function(africa) {
         .style('fill', function(d, i) {
             return colorScheme(Math.random());
         })
-        .on('click', function(d) {
-            if (d.properties.geounit == 'Sudan') {
-                console.log('Conditional executes when clicked on Sudan');
-            }
-            console.log('This country is called ' + d.properties.geounit);
-            console.log(d);
-        });
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 });
